0ba7ca5ad5e73260ad0b71ebd5a4117de4a7915f
unknown <urielgrif@gmail.com>
2021-05-06 09:58:33 -0300

    actualizacion a version a tp5b

--- myscript.js
+++ myscript.js
@@ -28,7 +28,17 @@ function buscar(newSearch) {
                 cantPaginas = Math.ceil(data.totalResults / 10);
                 if (data.Search) {
                     data.Search.forEach(el => {
-                        buscarDetalles(el.imdbID);
+                        lista.innerHTML += `
+                        <div class="pelicula">
+                            <img src=${el.Poster} alt="${el.Title}"/>
+                            <div>
+                                <h4>${el.Title}</h4>
+                                <p>Year: ${el.Year}</p>
+                                <p>Type: ${el.Type}</p>
+                                <button class="btn" onclick="buscarDetalles(\'${el.imdbID}\')" data-bs-toggle="modal" data-bs-target="#myModal">Ver Mas</button>
+                            </div>
+                        </div>
+                        `;
                     });
                 }
                 else {
@@ -36,39 +46,46 @@ function buscar(newSearch) {
                 }
             })
             .catch(err => console.log(err));
-
-        function buscarDetalles(id) {
-            fetch(`${urlAPI}&i=${id}`)
-                .then(res => res.json())
-                .then(data => {
-                    lista.innerHTML += `
-                        <div class="pelicula">
-                            <img src=${data.Poster} alt="${data.Title}"/>
-                            <div>
-                                <h4>${data.Title}</h4>
-                                <p>${data.Plot}</p>
-                                <p>Year: ${data.Year}</p>
-                                <p>Runtime: ${data.Runtime}</p>
-                                <p>Genre: ${data.Genre}</p>
-                                <p>IMDB rating: ${data.imdbRating}</p>
-                                <a href="https://www.imdb.com/title/${data.imdbID}/?ref_=nv_sr_srsg_0">Link a IMDB</a>
-                            </div>
-                        </div>
-                        `;
-                })
-                .catch(err => console.log(err));
-        }
     }
 
 }
+
+function buscarDetalles(id) {
+    let modalTitle = document.getElementById("exampleModalLabel");
+    let modalBody = document.getElementById("modal-info");
+
+    fetch(`${urlAPI}&i=${id}&plot=full`)
+        .then(res => res.json())
+        .then(data => {
+            modalTitle.innerHTML = `<h5>${data.Title}</h5>`;
+            modalBody.innerHTML = `
+                    <img src=${data.Poster} alt="${data.Title}"/>
+                    <div>
+                        <p>${data.Plot}</p>
+                        <p>Director: ${data.Director}</p>
+                        <p>Actors: ${data.Actors}</p>
+                        <p>Year: ${data.Year}</p>
+                        <p>Runtime: ${data.Runtime}</p>
+                        <p>Genre: ${data.Genre}</p>
+                        <p>Country: ${data.Country}</p>
+                        <p>IMDB rating: ${data.imdbRating}</p>
+                        <a href="https://www.imdb.com/title/${data.imdbID}/?ref_=nv_sr_srsg_0">Link a IMDB</a>
+                    </div>
+                `;
+        })
+        .catch(err => console.log(err));
+}
+
 
 function pasarPagina(num) {
     if (num === -1 && numPagina > 1) {
         numPagina += num;
         buscar(false);
+        window.scrollTo(0, 0);
     }
     if (num === 1 && numPagina < cantPaginas) {
         numPagina += num;
         buscar(false);
+        window.scrollTo(0, 0);
     }
 }
